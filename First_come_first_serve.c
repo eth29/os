030dbc20a727d1858b69881c995c8d8e9aa93ed2
eth29 <156825890+eth29@users.noreply.github.com>
2024-01-16 21:34:43 +0530

    Add files via upload

---
+++ First_come_first_serve.c
@@ -0,0 +1,39 @@
+/*First come first serve*/
+#include<stdio.h>
+  int main()
+ {
+    int n,bt[20],wt[20],tat[20],i,j;
+float avwt=0.0,avtat=0.0;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
+  printf("nEnter Process Burst Time\n");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+ wt[0]=0;   
+for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+printf("\nProcessttBurst \tWaitingTime \tTurnaround Time\n");
+ 
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+    }
+ 
+    avwt/=i;
+    avtat/=i;
+    printf("\n\nAverage Waiting Time:%fms\n",avwt);
+    printf("\nAverage Turnaround Time:%fms\n",avtat);
+ 
+    return 0;
+}
+
